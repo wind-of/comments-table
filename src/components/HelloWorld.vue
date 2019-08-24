73652b2fb2a10e114562f00937fd4aa0591260e4
ThWitness-| <glamorousfeces@mail.ru>
2019-08-24 10:21:50 +0300

    First commit

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,57 +1,164 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+    <div class="title">
+      <img alt="Vue logo" src="../assets/logo.png">
+      <h1>Comments' table</h1>
+    </div>
+    <div class="add-comments-form">
+      <div class="forms-wrapper">
+        <input placeholder="Name" type="text" v-model="commentState.author"><br>
+        <textarea placeholder="Write here a comment" v-model="commentState.content"></textarea><br>
+        <button @click="addComment">Add</button>
+      </div>
+    </div>
+    <div class="comments-wrapper">
+      <div class="comments">
+        <div class="comment" v-for="comment of comments" :key="comment.id">
+          <div class="content">
+            <h3>{{ comment.author }}</h3>
+            <div v-if="comment.isEditing">
+              <textarea v-model="comment.content"></textarea>
+              <br>
+              <button @click="toggleEdit(comment.id)">Save</button>
+            </div>
+            <h4 v-else>{{ comment.content }}</h4>
+          </div>
+          <div class="config">
+            <img v-show="!isEditingComment" src="../assets/edit.png"   alt="Edit"   @click="toggleEdit(comment.id)">
+            <img v-show="!isEditingComment" src="../assets/delete.png" alt="Delete" @click="deleteComment(comment.id)">
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
+
+
+
 
 <script>
 export default {
   name: 'HelloWorld',
-  props: {
-    msg: String
-  }
+  data(){
+    return{
+      isEditingComment: false,
+      commentState: {
+        author: '',
+        content: '',
+        id: 1,
+      },
+      comments: [
+        {
+          author: 'Oleg',
+          content: 'Lorem ipsum dolorvorem ipsum doorem ipsum doorem ipsum doorem ipsum doorem ipsum doorem ipsum doorem ipsum doorem ipsum doorem ipsum doorem ipsum doorem ipsum doorem ipsum doorem ipsum doorem ipsum doorem ipsum do sit amet adipisking elit.',
+          id: 0,
+          isEditing: false
+        }
+      ]
+    }
+  },
+  methods: {
+
+
+    addComment(){
+      let newComment = {};
+      Object.assign(newComment, this.commentState);
+      this.comments.unshift(newComment);
+
+      this.commentState.id++;
+      this.commentState.author = "";
+      this.commentState.content = ""; 
+    },
+
+
+    toggleEdit(id) {
+      const editingComment = this.comments.find(comment => comment.id == id);
+      editingComment.isEditing = !editingComment.isEditing;
+      this.isEditingComment = !this.isEditingComment;
+    },
+
+
+    deleteComment(id){
+      this.comments = this.comments.filter(comment => comment.id !== id)
+    }
+    
+  },
 }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
+
+
+
 <style scoped lang="scss">
-h3 {
-  margin: 40px 0 0;
+.title{
+  text-align: center;
 }
-ul {
-  list-style-type: none;
-  padding: 0;
+.add-comments-form{
+  text-align: center;
+  .forms-wrapper{
+    input{
+      font-size: 16px;
+      margin: 5px 15px;
+      padding: 10px 5px;
+      width: 10vw;
+      outline: none;
+      border: none;
+      border: 1px solid #999;
+      transition: .15s ease-in-out;
+    }
+    textarea{
+      font-size: 17px;
+      margin: 5px;
+      padding: 10px;
+      width: 30vw;
+      height: 5vw;
+      resize: none;
+      outline: none;
+      border: 1px solid grey;
+    }
+    button{
+      display: inline-block;
+      padding: 5px;
+      margin-left: auto;
+    }
+  }
 }
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
+.comments-wrapper{
+  width: 50%;
+  margin: 0 auto;
+  display: flex;
+  justify-content: space-around;
+  .comments{
+    margin: 20px 0;
+    .comment{
+      width: 45vw;
+      padding: 10px 20px;
+      border-bottom: .5px solid #bbb;
+      display: flex;
+      justify-content: center;
+      .content{
+        display: block;
+        h3{
+          color: #222;
+        }
+        textarea{
+          resize: none;
+          outline: none;
+          width: 40vw;
+          height: 5vw;
+          font-size: 17px;
+        }
+      }
+      .config{
+        padding: 15px 20px;
+        img{
+          width: 15px;
+          margin: 1px;
+          height: 15px;
+          cursor: pointer;
+        }
+      }
+    }
+  }
 }
 </style>
